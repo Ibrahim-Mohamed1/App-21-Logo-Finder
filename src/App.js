b13630a80269880a745f2a645defdf28407fbeaf
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-05-06 00:28:34 -0600

    logo finder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      search: ""
+    }
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  render() {
+    const styles={
+      form:{
+        textAlign:"center",
+        zoom: 2.3,
+      },
+      title:{
+        textAlign:"center", 
+        display:"block",
+        margin: 20,
+        color: "gold"
+      },
+      img:{
+        display:"block",
+        margin: "auto",
+        marginTop: 20,
+        border: 'solid black',
+        outline: "none"
+      }
+    }
+    return (
+      <div>
+        <h1 style={styles.title}>Find Company Logos</h1>
+        <form style={styles.form} onSubmit={this.handleSubmit} action="">
+          <input 
+            style={{outline:"none", borderRadius: 2, border: "white solid", textAlign:"center"}}
+            type="text" 
+            name="search" 
+            value={this.state.search}
+            onChange={this.handleChange}
+            autoFocus
+            autoComplete='off'
+            placeholder="Facebook.com"
+            required
+          />
+        </form>
+          {this.state.search === "" ? null :
+            <img style={styles.img} src={`https://logo.clearbit.com/${this.state.search}`} alt=""/>
+          }
+      </div>
+    );
+  }
 }
 
 export default App;
